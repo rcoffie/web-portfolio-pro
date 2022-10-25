069e994451aca1589ab69b6c3a5046bcfa616567
Robert Coffie <rcoffie22@yhahoo.com>
2022-10-25 13:08:55 +0000

    adding flash message to profile update page

--- account_engine/views.py
+++ account_engine/views.py
@@ -1,3 +1,4 @@
+from django.contrib import messages
 from django.contrib.auth import authenticate, login, logout
 from django.contrib.auth import views as auth_views
 from django.contrib.auth.decorators import login_required
@@ -43,7 +44,9 @@ def edit_profile(request, id):
             form = ProfileForm(request.POST, instance=profile)
             if form.is_valid():
                 form.save()
-                return redirect("home")
+                messages.success(request, 'profile updateed successfully')
+                # this will rediect back to the user's profile page 
+                return redirect(".")
         context = {"profile": profile, "form": form}
         return render(request, "edit_profile.html", context)
     else:
